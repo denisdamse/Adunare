76e0540773074c4bb3bbee6a4fa7ed866b5b96aa
denisdamse <146414688+denisdamse@users.noreply.github.com>
2023-10-16 23:13:27 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-
-Console.WriteLine("Hello, World!");
-Console.WriteLine(" ceau");
-
+namespace Adunare;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = Console.Read();
+        int j = Console.Read();
+        int k = i + j;
+        Console.WriteLine(k);
+    }
+}
 
